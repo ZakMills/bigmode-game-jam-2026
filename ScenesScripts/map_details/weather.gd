9e90f9a16ef8b3f4cebe6e2fbbc34c6904795bd8
ZakMills <Sephiroth@cfl.rr.com>
2026-02-02 15:02:18 -0500

    pushing updates

--- ScenesScripts/map_details/weather.gd
+++ ScenesScripts/map_details/weather.gd
@@ -1,13 +1,18 @@
-extends StaticBody2D
+extends Control
 
+var blizzard : bool = false
+var windy : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if (get_node_or_null("/ocean/Shape") != null):
-		$Shape.visible = Global.test
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func test():
+	#$AnimatedSprite2D.blur
+	#https://forum.godotengine.org/t/is-there-a-way-to-blur-a-sprite/21001/2
 	pass
